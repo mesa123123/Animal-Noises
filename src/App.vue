49d1d5945386f64abbe751fe192dac20ffbe9e46
Peter Bowman <bowmanpete123@gmail.com>
2018-12-09 21:25:17 +1300

    added multiple animals grid

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app">
-    <v-app id="barnYard" class="transparent">
+    <v-app 
+      id="barnYard" 
+      class="transparent">
       <v-container>
         <!-- Card to store the animal noise -->
-        <v-flex offset-md1 offset-sm1 offset-xs1 offset-lg1>
+        <v-flex
+          offset-md1
+          offset-sm1
+          offset-xs1
+          offset-lg1
+        >
           <h1
             id="animalTitle"
             justify-center
@@ -14,11 +21,7 @@
             text-lg-center
           >{{ animalPhrase }}</h1>
         </v-flex>
-        <v-flex justify-center align-center offset-md3 offset-sm3 offset-xs3 offset-lg3>
-          <v-responsive id="animalPic">
-            <v-img :src="centerPicture" v-on:click="centerPhrase" height="40vw" width="48vw"/>
-          </v-responsive>
-        </v-flex>
+        <FarmYard/>
       </v-container>
       <v-container app>
         <!-- Button Menu in lower corner -->
@@ -26,21 +29,21 @@
           <table id="animalsMenu">
             <td>
               <v-fab-transition>
-                <v-btn color="orange" fab v-show="menuOut" @click.native="selectAnimal('fox')">
+                <v-btn v-show="menuOut" color="orange" fab>
                   <v-icon>add</v-icon>
                 </v-btn>
               </v-fab-transition>
             </td>
             <td>
               <v-fab-transition>
-                <v-btn color="pink" fab v-show="menuOut" @click.native="selectAnimal('pig')">
+                <v-btn v-show="menuOut" color="pink" fab>
                   <v-icon>add</v-icon>
                 </v-btn>
               </v-fab-transition>
             </td>
             <td>
               <v-fab-transition>
-                <v-btn color="black" fab v-show="menuOut" @click.native="selectAnimal('cow')">
+                <v-btn v-show="menuOut" color="black" fab>
                   <v-icon color="white">add</v-icon>
                 </v-btn>
               </v-fab-transition>
@@ -58,19 +61,13 @@
 </template>
 
 <script>
-import pig_pic from "@/assets/images/pig.svg";
-import pigSound from "@/assets/audio/pig.mp3";
-import fox_pic from "@/assets/images/fox.svg";
-import foxSound from "@/assets/audio/fox.mp3";
-import cow_pic from "@/assets/images/cow.svg";
-import cowSound from "@/assets/audio/cow.mp3";
-
-const foxsSound = new Audio(foxSound);
-const cowsSound = new Audio(cowSound);
-const pigsSound = new Audio(pigSound);
+import FarmYard from "./components/FarmYard.vue";
 
 export default {
-  name: "app",
+  name: "App",
+  components: {
+    FarmYard
+  },
   data() {
     return {
       menuOut: false,
@@ -90,27 +87,6 @@ export default {
         this.firstSelection = true;
       }
       this.menuOut = !this.menuOut;
-    },
-    selectAnimal: function(inputAnimal) {
-      if (inputAnimal === "fox") {
-        this.centerPhrase = function(event) {
-          foxsSound.play();
-        };
-        this.centerPicture = fox_pic;
-        this.animalPhrase = "Fox says";
-      } else if (inputAnimal === "cow") {
-        this.centerPhrase = function(event) {
-          cowsSound.play();
-        };
-        this.centerPicture = cow_pic;
-        this.animalPhrase = "Cow says";
-      } else if (inputAnimal === "pig") {
-        this.centerPhrase = function(event) {
-          pigsSound.play();
-        };
-        this.centerPicture = pig_pic;
-        this.animalPhrase = "Pig says";
-      }
     }
   }
 };
--- src/Components/FarmYard.vue
+++ src/Components/FarmYard.vue
@@ -1,34 +1,57 @@
 <template>
-  <div>
-    <v-container>
-      <v-layout>
-        <v-card>
-          <v-card-media>
-            
-         </v-card-media>  
-        </v-card>  
-      </v-layout>
-    </v-container>
-  </div>
+  <v-container fill-height>
+    <v-flex (xs2| sm2 | md2 lg2 xl2)></v-flex>
+    <v-layout align-center justify-center row wrap>
+      <v-flex v-for="(animal) in barnyard" :key="animal" (xs5 | sm5 md5 lg5 xl5) ma-1>
+        <v-img :src="animal"/>
+      </v-flex>
+    </v-layout>
+    <v-flex (xs2| sm2 | md2 lg2 xl2)></v-flex>
+  </v-container>
 </template>
 
 <script>
-import pig_pic from "@/assets/images/pig.svg";
-import pigSound from "@/assets/audio/pig.mp3";
-import fox_pic from "@/assets/images/fox.svg";
-import foxSound from "@/assets/audio/fox.mp3";
-import cow_pic from "@/assets/images/cow.svg";
-import cowSound from "@/assets/audio/cow.mp3";
+import pigPic from "../assets/images/pig.svg";
+import pigSound from "../assets/audio/pig.mp3";
+import foxPic from "../assets/images/fox.svg";
+import foxSound from "../assets/audio/fox.mp3";
+import cowPic from "../assets/images/cow.svg";
+import cowSound from "../assets/audio/cow.mp3";
 
 const foxsSound = new Audio(foxSound);
 const cowsSound = new Audio(cowSound);
 const pigsSound = new Audio(pigSound);
 
 export default {
-  name: "farmYard",
-
+  name: "FarmYard",
+  data() {
+    return {
+      barnyard: [cowPic, cowPic, cowPic, cowPic]
+    };
+  },
+  methods: {
+    selectAnimal: function(inputAnimal) {
+      if (inputAnimal === "fox") {
+        this.centerPhrase = function(event) {
+          foxsSound.play();
+        };
+        this.animalPhrase = "Fox says";
+      } else if (inputAnimal === "cow") {
+        this.centerPhrase = function(event) {
+          cowsSound.play();
+        };
+        this.animalPhrase = "Cow says";
+      } else if (inputAnimal === "pig") {
+        this.centerPhrase = function(event) {
+          pigsSound.play();
+        };
+        this.animalPhrase = "Pig says";
+      }
+    }
+  }
 };
 </script>
+
 
 <style scoped>
 </style>
